7fc267cf8ed8a01dea49f17cd3fd942d15c08918
Tanay <tanay.duddalwar@gmail.com>
2023-10-21 22:01:17 +0530

    final

---
+++ myapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from myapp.models import Contact
+admin.site.register(Contact)
+# Register your models here.
---
+++ myapp/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
+from myapp.models import Contact
+import logging
+
+
+def home(request):
+    return render(request, "index.html")
+
+def aboutus(request):
+    if request.method == "POST":
+        try:
+            name = request.POST.get("name")
+            email = request.POST.get("email")
+            phone = request.POST.get("phone")
+            desc = request.POST.get("desc")
+            contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+            contact.save()
+            
+
+        # Save the contact object to the database
+        except IntegrityError as e:
+            logging.error(f"Error saving contact: {str(e)}")
+
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def service(request):
+    return render(request, "service.html")
